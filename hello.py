200068adf9031426d535b7c3d25d955e5c2d61ad
DAVID KRIER <dakrier@us.ibm.com>
2017-01-18 15:57:35 -0600

    add file

--- hello.py
+++ hello.py
@@ -11,13 +11,12 @@ app = Flask(__name__)
 # On Bluemix, get the port number from the environment variable PORT
 # When running this app on the local machine, default the port to 8080
 
-services = json.loads(os.getenv('VCAP_SERVICES'))
 port = int(os.getenv('PORT', 8080))
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! I am running on port ' + str(services)
+    return 'Hello World! I am running on port ' + str(port)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
